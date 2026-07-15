3bd4b4b814269d0232dbe98dac3b61833eec87df
Dryndatryntau <dryndatryntau@gmail.com>
2026-07-15 14:24:08 +0300

    Update Unit3.h

--- Unit3.h
+++ Unit3.h
@@ -1,64 +1,39 @@
-﻿#ifndef Unit3H
+#ifndef Unit3H
 #define Unit3H
 
 #include <System.Classes.hpp>
 #include <Vcl.Controls.hpp>
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
-#include <Vcl.ComCtrls.hpp>
 #include <Vcl.ExtCtrls.hpp>
-#include <Vcl.Grids.hpp>
 #include <Vcl.Dialogs.hpp>
+#include <vector>
 
-struct H4R_Failas {
-  String pavadinimas;
-  long offsetas;
-  long dydis;
+struct GlifoInfo {
+    long failo_offsetas;
+    int plotis;
+    int aukstis;
 };
 
 class TForm3 : public TForm
 {
 __published:
-	TPageControl *PageControl1;
-	TTabSheet *TabFonts;
-	TTabSheet *TabText;
-	TTabSheet *TabArch;
-	TPanel *Panel1;
-	TComboBox *ComboGame;
-	TComboBox *ComboLang;
-	TOpenDialog *OpenDialog1;
-	TSaveDialog *SaveDialog1;
-
-	// 🎯 SUTAISYTA ČIA: Grąžintas gamyklinis pavadinimas, kad dizaineris nepanikuotų!
 	TListBox *ListFonts;
-	TStringGrid *StringGrid1;
 	TImage *Image1;
-
-	TButton *BtnOpenFont;
-	TButton *BtnSaveFont;
 	TImage *Image2;
-
-	// Įvykių registracijos
-	void __fastcall FormCreate(TObject *Sender);
-	void __fastcall ComboLangChange(TObject *Sender);
-	void __fastcall ComboGameChange(TObject *Sender);
+	TButton *BtnOpenFont;
 	void __fastcall BtnOpenFontClick(TObject *Sender);
-	void __fastcall BtnSaveFontClick(TObject *Sender);
 	void __fastcall ListFontsClick(TObject *Sender);
-	void __fastcall Image2MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
-          int X, int Y);
-    	void __fastcall Image2MouseMove(TObject *Sender, TShiftState Shift, int X, int Y);
-
+	void __fastcall Image2MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift, int X, int Y);
+	void __fastcall Image2MouseMove(TObject *Sender, TShiftState Shift, int X, int Y);
 private:
+	std::vector<char> g_srifto_buf;
+	long g_srifto_dydis;
+	std::vector<GlifoInfo> g_glifu_indeksas;
 public:
 	__fastcall TForm3(TComponent* Owner);
-	void rodyk(int sr_sk);
-	void __fastcall UzkrautiKalba(String kalbos_failas);
-
-
 };
 
 extern PACKAGE TForm3 *Form3;
 
 #endif
-
